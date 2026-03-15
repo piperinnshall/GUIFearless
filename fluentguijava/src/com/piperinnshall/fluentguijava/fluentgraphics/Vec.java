14285ff565099d2eec559ab123e4c4ebd592b42c
Piper Inns Hall <piper.innshall@gmail.com>
2026-03-15 16:49:00 +1300

    chore: Add Vec3

--- fluentguijava/src/com/piperinnshall/fluentguijava/fluentgraphics/Vec.java
+++ fluentguijava/src/com/piperinnshall/fluentguijava/fluentgraphics/Vec.java
@@ -3,7 +3,7 @@ package com.piperinnshall.fluentguijava.fluentgraphics;
 import java.awt.Dimension;
 import java.awt.Point;
 
-/* 
+/*
  * Library-only point (do not expose to library user)
  */
 record Point2(int x, int y) {
@@ -15,8 +15,6 @@ record Point2(int x, int y) {
 }
 
 interface Vec<T extends Vec<T>> {
-  float x();
-  float y();
   T add(T point);
   T sub(T point);
   T add(float scalar);
@@ -24,10 +22,11 @@ interface Vec<T extends Vec<T>> {
   T div(float scalar);
   T mul(float scalar);
   float dot(Vec<T> v);
-
   default T normalize() { return div(len()); }
   default float len() { return (float) Math.sqrt(lenSq()); }
   default float lenSq() { return dot(this); }
+  default float dist(T other) { return sub(other).len(); }
+  default float distSq(T other) { return sub(other).lenSq(); }
 }
 
 record Vec2(float x, float y) implements Vec<Vec2> {
@@ -53,6 +52,38 @@ record Vec2(float x, float y) implements Vec<Vec2> {
     return new Vec2(x / scalar, y / scalar);
   }
   public float dot(Vec<Vec2> v) {
-    return x * v.x() + y * v.y();
+    var v2 = (Vec2) v;
+    return x * v2.x() + y * v2.y();
+  }
+}
+
+record Vec3(float x, float y, float z) implements Vec<Vec3> {
+  Vec3(float scalar) {
+    this(scalar, scalar, scalar);
+  }
+  public Vec3 add(Vec3 point) {
+    return new Vec3(x + point.x, y + point.y, z + point.z);
+  }
+  public Vec3 sub(Vec3 point) {
+    return new Vec3(x - point.x, y - point.y, z - point.z);
+  }
+  public Vec3 add(float scalar) {
+    return new Vec3(x + scalar, y + scalar, z + scalar);
+  }
+  public Vec3 sub(float scalar) {
+    return new Vec3(x - scalar, y - scalar, z - scalar);
+  }
+  public Vec3 mul(float scalar) {
+    return new Vec3(x * scalar, y * scalar, z * scalar);
+  }
+  public Vec3 div(float scalar) {
+    return new Vec3(x / scalar, y / scalar, z / scalar);
+  }
+  public float dot(Vec<Vec3> v) {
+    var v3 = (Vec3) v;
+    return x * v3.x() + y * v3.y() + z * v3.z();
+  }
+  public Vec3 cross(Vec3 v) {
+    return new Vec3(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x);
   }
 }
